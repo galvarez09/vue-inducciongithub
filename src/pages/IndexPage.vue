81f0a083738b0394f184b988526766370032ab3b
Giancarlo Joel Alvarez Gallardo <galvarezga09@outlook.com>
2025-05-04 23:29:06 -0500

    Cambio de titulo de prueba en github

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <example-component
-      title="Example component"
+      title="Hola Mundo en github repositorio"
       active
       :todos="todos"
       :meta="meta"
@@ -17,27 +17,27 @@ import ExampleComponent from 'components/ExampleComponent.vue';
 const todos = ref<Todo[]>([
   {
     id: 1,
-    content: 'ct1'
+    content: 'ct1',
   },
   {
     id: 2,
-    content: 'ct2'
+    content: 'ct2',
   },
   {
     id: 3,
-    content: 'ct3'
+    content: 'ct3',
   },
   {
     id: 4,
-    content: 'ct4'
+    content: 'ct4',
   },
   {
     id: 5,
-    content: 'ct5'
-  }
+    content: 'ct5',
+  },
 ]);
 
 const meta = ref<Meta>({
-  totalCount: 1200
+  totalCount: 1200,
 });
 </script>
